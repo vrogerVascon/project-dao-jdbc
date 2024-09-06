a04b5682f430383390985625a67a446531e87f50
vrogerVascon <vrogerio87@gmail.com>
2024-09-06 10:12:31 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import model.endities.Department;
+
+public class Program {
+    public static void main(String[] args) {
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+        
+    }
+
+}
