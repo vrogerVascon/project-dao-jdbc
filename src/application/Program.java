02137c44eb94da878774778eae51f4e8d6b4fcf7
vrogerVascon <vrogerio87@gmail.com>
2024-09-07 07:42:55 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,15 @@
 package application;
 
 import model.endities.Department;
+import model.endities.Seller;
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        Seller seller = new Seller(3, "Bob", "bob@gmail.com", new Date(), 2000.0, obj);  
         
+        System.out.println(seller);
     }
 
 }
